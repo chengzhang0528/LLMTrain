a5a76d671c12a38021b215538f0d2a1aca759492
cheng.zhang <cheng.zhang@cri-united-imaging.com>
2026-08-07 14:13:40 +0800

    Deploy LLMTrain site

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.lXo-4BCy.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.C6ueLaXS.js";import{_ as a}from"./mermaid.core.Cgfv-P0H.js";import"./chunk-5VM5RSS4.CVlQlxPX.js";import"./chunk-XXDRQBXY.DLEQIbEO.js";import"./chunk-KBJHAD2P.BZsQ7SOI.js";import"./chunk-2GRJ4B5K.BuopgKy_.js";import"./channel.BTwDj2NT.js";import"./framework.BqNd1sPY.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
